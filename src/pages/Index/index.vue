e2cb68f77b86abfac29751d442fdb0cc1fb2ba3f
Zero <silverfantacy@gmail.com>
2018-10-05 11:31:23 +0800

    增加 nav 版面修正

--- src/pages/Index/index.vue
+++ src/pages/Index/index.vue
@@ -21,6 +21,21 @@ export default {
   },
   mounted() {
     // document.location.href = 'https://nutraceutical.org.tw/'
+
+    var body = document.body
+    var burgerMenu = document.getElementsByClassName('b-menu')[0]
+    var burgerContain = document.getElementsByClassName('b-container')[0]
+    var burgerNav = document.getElementsByClassName('b-nav')[0]
+
+    burgerMenu.addEventListener(
+      'click',
+      function toggleClasses() {
+        ;[body, burgerContain, burgerNav].forEach(function(el) {
+          el.classList.toggle('open')
+        })
+      },
+      false
+    )
   },
   created() {
     this.getAgenda()
